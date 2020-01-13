a1feee901a5c5bf8c218adc05b0692528623ea7b
EC2 Default User <ec2-user@ip-172-31-16-111.us-east-2.compute.internal>
2020-01-13 19:54:06 +0000

    works but without jump

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -7,42 +7,100 @@ game_state.main = function() {};
 game_state.main.prototype = {
 
     preload: function() {
-        game.load.image('player', 'assets/player.png');
-        game.load.image('object', 'assets/object.png');
+       game.load.image('sky', 'assets/sky.png');
+       game.load.image('ground', 'assets/green platform.png');
+       game.load.image('paper', 'assets/Paper.png', 58, 74);
+       game.load.spritesheet('player', 'assets/Dog.png', 100, 100);
     },
 
     create: function() {
-        game.stage.backgroundColor = '#3598db';
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.player = game.add.sprite(200, 400, 'player');
-        game.physics.arcade.enable(this.player);
-        this.player.enableBody = true;
-        this.player.body.immovable = true;
-        this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-        this.objects = game.add.group();
-        this.objects.enableBody = true;
-        var _this = this;
-        setInterval(function() {
-            var object = _this.objects.create(Math.random() * 800, -64, 'object');
-            object.body.gravity.y = 300;
-        }, 1000) 
+       game.physics.startSystem(Phaser.Physics.ARCADE);
+
+       this.score = 0;
+       game.add.sprite(0, 0, 'paper');
+       // game.add.sprite(0, 0, 'sky');
+       game.stage.backgroundColor = '#c1cad6';
+
+       this.platforms = game.add.group();
+       this.platforms.enableBody = true;
+       
+        //game.stage.backgroundColor = '#000000';
+        //this.text = game.add.text(
+          // 20, 20,
+          //  'This story begins with a dog named Kai. Kai was living on the streets when he overheard some evil men plotting to kill 10 people! He decided he would have to stop them. Unfortunately, Kai does not know enough about their plan yet. Help Kai collect papers which will give him more info about the bad guys plan.', {
+          // fontSize: '30px',
+          // fill: '#fff'
+          // }
+       // );
+     // game.input.keyboard.onPressCallback = function(e) {
+        //  console.log("key pressed: ", e);
+        //  if (e == 's') {
+         //     game.input.keyboard.onPressCallback = null;
+          //    game.state.start('main');
+        //  }
+    //  }
+
+       var ground = this.platforms.create(0, game.world.height - 64, 'ground');
+       ground.scale.setTo(2, 2);
+       ground.body.immovable = true;
+
+       var ledge = this.platforms.create(20, 150, 'ground');
+       ledge.body.immovable = true;
+
+       this.player = game.add.sprite(32, game.world.height = 150, 'player');
+       this.player.scale.setTo(1.1);
+       game.physics.arcade.enable(this.player);
+       this.player.body.bounce.y = 0.1
+       this.player.body.gravity.y = 300
+       this.player.body.collideWorldBounds = true;
+       
+       this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+       this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+//       game.physics.arcade.collide(this.player, this.platforms);
+    
+       this.cursors = game.input.keyboard.createCursorKeys();
+       this.papers = game.add.group();
+       this.papers.enableBody = true;
+       for (var i = 0; i < 12; i++) {
+           var paper = this.papers.create(i * 70, 0, 'paper');
+           this.papers.scale.setTo(.75);
+           paper.body.gravity.y = 300;
+           paper.body.bounce.y = 0.7 + Math.random() * 0.2;
+       }
+       this.scoreText = game.add.text(16, 16, 'score: 0', {
+           fontSize: '32px', 
+           fill: '#000'
+       });
     },
 
     update: function() {
-        if (this.left.isDown) {
-            this.player.body.velocity.x = -300;
+        this.player.body.velocity.x = 0;
+        if (this.cursors.left.isDown) {
+            this.player.body.velocity.x = -150;
+            this.player.animations.play('left');
         }
-        else if (this.right.isDown) {
-            this.player.body.velocity.x = 300;
+        else if (this.cursors.right.isDown) {
+            this.player.body.velocity.x = 150;
+            this.player.animations.play('right');
         }
         else {
-            this.player.body.velocity.x = 0;
+            this.player.animations.stop();
+            this.player.frame = 4;
         }
-        game.physics.arcade.overlap(this.player, this.objects, this.hitObject, null, this);
+        if (this.cursors.up.isDown && this.player.body.touching.down){
+            alert('test');
+            this.player.body.velocity.y = -350;
+        }
+        game.physics.arcade.collide(this.player, this.platforms);
+        game.physics.arcade.collide(this.papers, this.platforms);
+        game.physics.arcade.overlap(this.player, this.papers, this.collectPaper, null, this);
+        this.scoreText.text = "score: " + this.score;
     },
-    hitObject: function(player, object){
-        object.kill();
+   // game.state.add('intro', game_state.intro);
+    collectPaper: function(player, paper) {
+        paper.kill();
+        this.score += 1;
     }
 }
 game.state.add('main', game_state.main);
