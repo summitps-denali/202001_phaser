71f834d9ea587a6030f9627cf266e9dff9d241c9
EC2 Default User <ec2-user@ip-172-31-0-47.us-east-2.compute.internal>
2020-01-14 19:48:39 +0000

    added reference intro page

--- myGame2/myGame2.js
+++ myGame2/myGame2.js
@@ -1,7 +1,4 @@
-/*global Phaser*/
-
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
-var game_state = {}
+/*  global game game_state  */
 
 game_state.main = function() {};
 game_state.main.prototype = {
@@ -117,4 +114,3 @@ game_state.main.prototype = {
 
 }
 game.state.add('main', game_state.main);
-game.state.start('main');
