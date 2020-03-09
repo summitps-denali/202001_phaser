30bdf5870fdef8331d2279264edda21ae70e7f48
EC2 Default User <ec2-user@ip-172-31-5-168.us-east-2.compute.internal>
2020-03-09 03:34:30 +0000

    rtian f1

---
+++ boss.js
@@ -0,0 +1,21 @@
+class Bullet extends Phaser.GameObjects.Sprite {
+// constructor(scene) {
+
+//     var x = 0;
+//     var y = 0;
+//     super(scene, x, y, "boss"); //parameters
+//     scene.add.existing(this); // add object to scene
+//     //type characteristics below
+//     scene.physics.world.enableBody(this); //add physics
+//     this.body.allowGravity = false; //gravity?
+//     this.setOrigin(0.5, 0.5); //origin?
+//     //this.body.velocity.x = 500;
+// }
+// update() {
+//     //this.body.velocity.x = 500;
+//     if (player.x < this.x) {
+//         alert("hello world");
+//     }
+
+// }//end of update
+}
